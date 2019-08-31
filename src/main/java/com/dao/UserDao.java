71dcf76482a2e57873f22af4d82028c6910578fb
liuxi1211 <201402321@qq.com>
2019-08-31 10:12:12 +0800

    1011版本更新

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -11,5 +11,8 @@ public interface UserDao {
     public List<String> queryListA(String nameB);
     public List<String> queryListA();
     public List<String> queryListB(String nameA);
+    public List<String> queryListb(String nameA);
+    public List<String> queryListd(String nameA);
+    public List<String> queryList11(String nameA);
     List<DistRelaBean> getDeptListByType(HashMap<String,String> hashMap);
 }
